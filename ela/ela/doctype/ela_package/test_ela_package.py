33e3156cfc9718c1ac3b39dadb80293c2a34c4b5
chandraitfc <chandra@itforchange.net>
2025-12-11 16:07:23 +0530

    Added ela package init

---
+++ ela/ela/doctype/ela_package/test_ela_package.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, IT for Change and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestELAPackage(FrappeTestCase):
+	pass
