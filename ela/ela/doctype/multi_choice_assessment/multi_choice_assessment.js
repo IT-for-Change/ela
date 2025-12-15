7b7d8c4dd620b53af10b6b5a19cce49567c8d0a1
chandraitfc <chandra@itforchange.net>
2025-12-15 07:31:37 +0530

    Choices as separate linked assessments, not inline

---
+++ ela/ela/doctype/multi_choice_assessment/multi_choice_assessment.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, IT for Change and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Multi Choice Assessment", {
+// 	refresh(frm) {
+
+// 	},
+// });
