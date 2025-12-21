1f4eebbcd4a3303263ca1256c8a699085dc33cc4
chandraitfc <chandra@itforchange.net>
2025-12-21 07:00:33 +0530

    Better UX: the teacher can now create the assessment form right from the activity screen. This is how this should be demo'ed and used going forward. The activity id and title will be auto-filled, activity id hidden.

--- ela/ela/doctype/activity/activity.js
+++ ela/ela/doctype/activity/activity.js
@@ -1,8 +1,13 @@
 // Copyright (c) 2025, IT for Change and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Activity", {
-// 	refresh(frm) {
+frappe.ui.form.on("Activity", {
+	refresh(frm) {
 
-// 	},
-// });
+	},
+
+    create_assessment_form(frm) {
+        url = "/app/assessment-form/new?activity=" + frm.doc.name + "&activity_title=" + frm.doc.title;
+        window.open(url, "_blank");
+    }
+});
