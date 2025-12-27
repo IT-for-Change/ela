d1c251c72ea7523a7a3e27ee449ac0e0f079a347
chandraitfc <chandra@itforchange.net>
2025-12-27 11:13:11 +0530

    PoC for activity page async integration with elamid service. Feature to be implemented.

--- ela/ela/doctype/activity/activity.js
+++ ela/ela/doctype/activity/activity.js
@@ -1,11 +1,42 @@
 // Copyright (c) 2025, IT for Change and contributors
 // For license information, please see license.txt
 
+function waiting(frm) {
+    var wrapper = $(frm.fields_dict['pending_assessment_count_msg'].wrapper);
+    wrapper.html("<h4>Waiting...</h4>");
+}
+
 frappe.ui.form.on("Activity", {
-	refresh(frm) {
 
-	},
+    refresh(frm) {
 
+        waiting(frm);
+
+        frappe.call({
+            method: "ela.ela.doctype.activity.activity.display_assessment_block",
+            args: { activity_id: frm.doc.activity_id },
+            callback(r) {
+                if (r.message > 0) {
+                    var wrapper = $(frm.fields_dict['pending_assessment_count_msg'].wrapper);
+                    wrapper.html("<h4>" + r.message + " submissions pending assessment</h4>");
+                    frm.toggle_display('run_assessment', true);
+                }
+            }
+        });
+    },
+
+    run_assessment(frm) {
+        frappe.call({
+            method: "ela.ela.doctype.activity.activity.run_assessment",
+            args: { activity_id: frm.doc.activity_id },
+            callback(r) {
+                frappe.msgprint(__(r));
+            }
+        });
+
+    },
+
+    //fn name matches the button name in the activity form page
     create_assessment_form(frm) {
         url = "/app/assessment-form/new?activity=" + frm.doc.name + "&activity_title=" + frm.doc.title;
         window.open(url, "_blank");
--- ela/ela/doctype/activity/activity.py
+++ ela/ela/doctype/activity/activity.py
@@ -3,6 +3,9 @@
 
 # import frappe
 from frappe.model.document import Document
+import frappe
+import requests
+import time
 
 
 class Activity(Document):
@@ -10,3 +13,28 @@ class Activity(Document):
     def before_save(self):
         if (self.activity_id is None):
             self.activity_id = self.name
+
+
+@frappe.whitelist()
+def display_assessment_block(activity_id):
+    records = frappe.db.count('Learner Submission', {
+        'status': 'Submitted', 'activity_eid': activity_id})
+    # frappe.msgprint(f'found {records} submissions')
+    time.sleep(10)
+    return records
+
+
+@frappe.whitelist()
+def run_assessment(activity_id):
+    api_url = "http://elamid:80/run"
+    params = {'id': activity_id} if activity_id else {}
+
+    # Make the GET request to the external Flask API
+    response = requests.get(api_url, params=params)
+
+    # Check for successful response
+    if response.status_code == 200:
+        return response.text  # or any other type of response data you need
+    else:
+        # Handle API failure
+        return f"Error: {response.status_code} - {response.text}"
