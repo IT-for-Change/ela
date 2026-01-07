c457abecaddd08bdb30077f45bbd9fea15501000
chandraitfc <chandra@itforchange.net>
2026-01-07 15:38:15 +0530

    Major upgrade for conversation speech separation integration with elaai

--- ela/ela/doctype/activity/activity.js
+++ ela/ela/doctype/activity/activity.js
@@ -2,13 +2,29 @@
 // For license information, please see license.txt
 
 function waiting(frm) {
+
     var wrapper = $(frm.fields_dict['pending_assessment_count_msg'].wrapper);
-    wrapper.html("<h4>Waiting...</h4>");
+    wrapper.html("<h4>Processing...</h4>");
+}
+
+function run_assessment(frm, op) {
+    frappe.call({
+        method: "ela.ela.doctype.activity.activity.run_assessment",
+        args: {
+            activity_id: frm.doc.activity_id,
+            operation: op
+        },
+        callback(r) {
+            frappe.msgprint(__(r));
+        }
+    });
+
 }
 
 frappe.ui.form.on("Activity", {
 
     refresh: function (frm) {
+
         frm.add_custom_button(__('Refresh Assessment Status'), function () {
             waiting(frm);
 
@@ -17,22 +33,55 @@ frappe.ui.form.on("Activity", {
                 args: { activity_eid: frm.doc.activity_id },
                 callback(r) {
                     var wrapper = $(frm.fields_dict['pending_assessment_count_msg'].wrapper);
-                    wrapper.html("<h4>" + r.message + " submission(s) pending assessment. See assessment log tab for details.</h4>");
-                    frm.toggle_display('run_assessment', true);
+                    wrapper.html("<h4>" + r.message['total_pending'] + " submission(s) pending assessment. " + "</h4><br/><p>" +
+                        r.message['count_speech_separation'] + " submission(s) pending speech separation. " + "<br/>" +
+                        r.message['count_language_check'] + " submission(s) pending language check. " + "<br/>" +
+                        r.message['count_transcription'] + " submission(s) pending transcription. " + "<br/>" +
+                        r.message['count_text_analysis'] + " submission(s) pending text analysis." + "<br/>" +
+                        r.message['count_report'] + " submission(s) pending reporting." + "<br/><br/>" +
+                        "See assessment log tab for details.</p>");
+                    //unhide the field now.
+                    frm.set_df_property('pending_assessment_count_msg', 'hidden', 0);
+
+                    if (r.message['count_speech_separation'] == 0) {
+                        frm.toggle_display('run_speech_separation', false);
+                    }
+                    if (r.message['count_language_check'] == 0) {
+                        frm.toggle_display('run_language_check', false);
+                    }
+                    if (r.message['count_transcription'] == 0) {
+                        frm.toggle_display('run_speech_to_text', false);
+                    }
+                    if (r.message['count_text_analysis'] == 0) {
+                        frm.toggle_display('run_text_analysis', false);
+                    }
+                    if (r.message['count_report'] == 0) {
+                        frm.toggle_display('run_reporting', false);
+                    }
+
                 }
             });
         });
     },
 
-    run_assessment(frm) {
-        frappe.call({
-            method: "ela.ela.doctype.activity.activity.run_assessment",
-            args: { activity_id: frm.doc.activity_id },
-            callback(r) {
-                frappe.msgprint(__(r));
-            }
-        });
+    run_speech_separation(frm) {
+        run_assessment(frm, "sdz")
+    },
 
+    run_language_check(frm) {
+        run_assessment(frm, "langid")
+    },
+
+    run_speech_to_text(frm) {
+        run_assessment(frm, "stt")
+    },
+
+    run_text_analysis(frm) {
+        run_assessment(frm, "nlp")
+    },
+
+    run_reporting(frm) {
+        run_assessment(frm, "report")
     },
 
     //fn name matches the button name in the activity form page
--- ela/ela/doctype/activity/activity.py
+++ ela/ela/doctype/activity/activity.py
@@ -15,7 +15,7 @@ class Activity(Document):
             self.activity_id = self.name
 
 
-def refresh_submission_status(submissions):
+def refresh_submission_status(activity_eid):
 
     submissions = frappe.get_list("Learner Submission",
                                   filters={'activity_eid': activity_eid})
@@ -39,15 +39,18 @@ def refresh_submission_status(submissions):
                         if (assessment_doc.conversation == 1):
                             question.status = 'PENDING_LEARNER_SPEECH_SEPARATION'
                             submission_doc.save()
+                            # frappe.msgprint(
+                            # f'{submission_doc.name} is pending speech separation')
                             continue
-                        if (assessment_doc.detect_language == 1):
-                            question.status = 'PENDING_LANGUAGE_CHECK'
-                            submission_doc.save()
-                            continue
-                        # if nota, then set to proceed to transcription
-                        question.status = 'PENDING_TRANSCRIPTION'
-                        submission_doc.save()
-                        continue
+                        if (assessment_doc.conversation == 0):
+                            if (assessment_doc.detect_language == 1):
+                                question.status = 'PENDING_LANGUAGE_CHECK'
+                                submission_doc.save()
+                                continue
+                            else:
+                                question.status = 'PENDING_TRANSCRIPTION'
+                                submission_doc.save()
+                                continue
 
                     if (question.status == 'LEARNER_SPEECH_SEPARATION_COMPLETE'):
                         if (assessment_doc.detect_language == 1):
@@ -75,7 +78,10 @@ def refresh_submission_status(submissions):
                         continue
 
 
-def update_activity_assessment_log_view(submissions, activity_eid):
+def update_activity_assessment_log_view(activity_eid):
+
+    submissions = frappe.get_list("Learner Submission",
+                                  filters={'activity_eid': activity_eid})
 
     activity_doc = frappe.get_list(doctype="Activity", filters={
         'activity_id': activity_eid}, limit_page_length=1)
@@ -99,12 +105,13 @@ def update_activity_assessment_log_view(submissions, activity_eid):
 
         for index, question in enumerate(questions_output):
 
-            ui_assessment_log_name = ''  # this will hold the assessment log UI child table name
+            # this will hold the assessment log UI child table name
+            ui_assessment_log_name = 'NA'
             submission_entry = {}
-            submission_entry['submission']: submission_doc.name
-            submission_entry['output']: question
-            submission_entry['learner']: submission_doc.learner
-            submission_entry['teacher']: submission_doc.teacher_reference
+            submission_entry['submission'] = submission_doc.name
+            submission_entry['output'] = question
+            submission_entry['learner'] = submission_doc.learner
+            submission_entry['teacher'] = submission_doc.teacher_reference
 
             if question.status == 'PENDING_LEARNER_SPEECH_SEPARATION':
                 ui_assessment_log_name = "speech_separation"
@@ -117,44 +124,67 @@ def update_activity_assessment_log_view(submissions, activity_eid):
             if question.status == 'PENDING_REPORT':
                 ui_assessment_log_name = "report"
 
-            activity_doc.append(assessment_type_name, submission_entry)
+            activity_doc.append(ui_assessment_log_name, submission_entry)
 
     activity_doc.save()
+
+    count_speech_separation = len(activity_doc.speech_separation)
+    count_language_check = len(activity_doc.language_identification)
+    count_transcription = len(activity_doc.transcription)
+    count_text_analysis = len(activity_doc.text_analysis)
+    count_report = len(activity_doc.report)
+
+    return {
+        'count_speech_separation': count_speech_separation,
+        'count_language_check': count_language_check,
+        'count_transcription': count_transcription,
+        'count_text_analysis': count_text_analysis,
+        'count_report': count_report,
+        'total_pending': count_speech_separation +
+        count_language_check +
+        count_transcription +
+        count_text_analysis +
+        count_report
+    }
 
 
 @frappe.whitelist()
 def display_assessment_block(activity_eid):
 
     # update submission docs status for next action based on previous action done via elamid
-    refresh_submission_status(submissions)
+    submissions_count = refresh_submission_status(activity_eid)
     # update UI view in activity
-    update_activity_assessment_log_view(submissions, activity_doc)
+    count_stats = update_activity_assessment_log_view(activity_eid)
 
-    submissions_count = len(submissions)
-    return submissions_count
+    return count_stats
 
 
 @frappe.whitelist()
 def run_assessment(activity_id, operation):
 
-    doc = frappe.get_doc('ELA Configuration')
+    doc = frappe.get_doc('ELAConfiguration')
     elamid_url = doc.middleware_endpoint
 
     ela_api = doc.speech_separation_callback
     ela_api_host = doc.host
     ela_api_port = doc.port
     ela_image = doc.speech_separation
+    ela_ai_install_dir = doc.ai_install_dir
+    ela_api_token = doc.ela_api_token
     ela_activity = activity_id
+
     elamid_params = {
         'ela_image': ela_image,
-        'ela_activity': activity_id,
         'ela_api': ela_api,
         'ela_api_host': ela_api_host,
-        'ela_api_port': ela_api_port
+        'ela_api_port': ela_api_port,
+        'ela_ai_install_dir': ela_ai_install_dir,
+        'ela_ai_operation': operation,
+        'ela_activity': ela_activity,
+        'ela_api_token': ela_api_token
     }
 
     response = requests.get(elamid_url, params=elamid_params)
-
     if response.status_code == 200:
         return response.text
     else:
@@ -162,9 +192,9 @@ def run_assessment(activity_id, operation):
 
 
 @frappe.whitelist()
-def get_submissions(activity_eid, reason):
+def get_submissions(activity_eid, operation):
 
-    doc = frappe.get_doc('ELA Configuration')
+    doc = frappe.get_doc('ELAConfiguration')
     host = doc.host
     port = doc.port
     response = {"items": []}
@@ -175,6 +205,8 @@ def get_submissions(activity_eid, reason):
 
     for submission in submissions:
         doc = frappe.get_doc("Learner Submission", submission)
+        teacher_doc = frappe.get_doc("Teacher", doc.teacher_reference)
+        learner_doc = frappe.get_doc("Learner", doc.learner)
         item_obj = {
             "item_key": submission,
             "entries": []
@@ -182,10 +214,28 @@ def get_submissions(activity_eid, reason):
         question_outputs = doc.response
         for index, question in enumerate(question_outputs):
             if question.type == 'AUDIO':
-                entry_obj = {
-                    "entry_key": question.file,
-                    "language": "en",
+                stt = {
+                    "language": "EN",
                     "source": f"{host}:{port}{question.file}"
+                }
+                lang_check = {
+                    "language_candidate_s": f"EN,{learner_doc.primary_home_language}",
+                    "source": f"{host}:{port}{question.file}"
+                }
+                sdz = {
+                    "source": f"{host}:{port}{question.file}",
+                    "source_separation_ref": f"{host}:{port}{teacher_doc.voice_sample}"
+                }
+                nlp = {
+                    "source": "<asr_text>",
+                    "grammar": "1"
+                }
+                entry_obj = {
+                    "key": question.file,
+                    "sdz": sdz,
+                    "lang_check": lang_check,
+                    "stt": stt,
+                    "nlp": nlp
                 }
             else:
                 continue
@@ -196,26 +246,57 @@ def get_submissions(activity_eid, reason):
     return response
 
 
+def update_question_status(submission, entry_key, status):
+    question_outputs = submission.response
+    for index, question in enumerate(question_outputs):
+        if question.type == 'AUDIO':
+            if question.file == entry_key:
+                question.status = status
+
+
 @frappe.whitelist()
-def update_submissions(outputs):
+def update_submissions(outputs, operation):
 
     try:
         outputs = frappe.parse_json(outputs)
-
         for index, output in enumerate(outputs):
             learner_submission_id = output["item_key"]
-            audio_file = output["entry_key"]
-            asr_text = output["asr_text"]
-            language = output["language"]
-            confidence = output["confidence"]
             submission = frappe.get_doc(
                 "Learner Submission", learner_submission_id)
-            submission.append("assessment_outputs", {
-                "language_identified": language,
-                "confidence": confidence,
-                "learner_contribution": audio_file,
-                "asr_text": asr_text
-            })
+            assessment_output = {}
+            if operation == "sdz":
+                assessment_output = output["sdz"]
+                learner_duration = assessment_output['learner_duration']
+                learner_max_duration = assessment_output['learner_max_duration']
+                teacher_duration = assessment_output['teacher_duration']
+                teacher_max_duration = assessment_output['teacher_max_duration']
+                total_turns = assessment_output['total_turns']
+                audio_fileid_learner = assessment_output['audio_fileid_learner']
+                # TODO - submission assessment_outputs reset before appending new.
+                submission.append("assessment_outputs", {
+                    'audio_response_recording': output["entry_key"],
+                    'learner_duration': learner_duration,
+                    'learner_duration_max': learner_max_duration,
+                    'teacher_duration': teacher_duration,
+                    'teacher_duration_max': teacher_max_duration,
+                    'total_turns': total_turns,
+                    'learner_speech_diarized': audio_fileid_learner
+                })
+                update_question_status(
+                    submission, output["entry_key"], 'LEARNER_SPEECH_SEPARATION_COMPLETE')
+
+            if operation == "stt":
+                asr_text = output["asr_text"]
+                language = output["language"]
+                confidence = output["confidence"]
+
+                submission.append("assessment_outputs", {
+                    "language_identified": language,
+                    "confidence": confidence,
+                    "learner_contribution": audio_file,
+                    "asr_text": asr_text
+                })
+
             submission.save()
             frappe.db.commit()
     except Exception as e:
