fb4a7664235ee385996cd995d5afd60385f19dbe
chandraitfc <chandra@itforchange.net>
2026-03-10 22:35:37 +0530

    report operation enhancements

--- ela/ela/api/submissions.py
+++ ela/ela/api/submissions.py
@@ -123,12 +123,24 @@ def get_submissions(activity_eid, operation):
                     "language": assessment_output_row.transcription_language if assessment_output_row is not None else '',
                     "grammar": "0"
                 }
+                report = {
+                    "transcription_language": assessment_output_row.transcription_language if assessment_output_row is not None else '-',
+                    "transcription_language_reason": assessment_output_row.transcription_language_reason if assessment_output_row is not None else 'LANGID_NO_SPEECH',
+                    "asr_text": assessment_output_row.asr_text if assessment_output_row is not None else '',
+                    "hallu_score":  assessment_output_row.hallu_score if assessment_output_row is not None else 0,
+                    "word_count": assessment_output_row.word_count if assessment_output_row is not None else 0,
+                    "lexical_density": assessment_output_row.lexical_density if assessment_output_row is not None else 0,
+                    "text_analysis": assessment_output_row.nlp_text_analysis if assessment_output_row is not None else '',
+                    "learner_duration": assessment_output_row.learner_duration if assessment_output_row is not None else 0,
+                    "teacher_duration": assessment_output_row.teacher_duration if assessment_output_row is not None else 0
+                }
                 entry = {
                     "key": question.file,
                     "sdz": sdz,
                     "langid": langid,
                     "stt": stt,
-                    "nlp": nlp
+                    "nlp": nlp,
+                    "report": report
                 }
             else:
                 continue
@@ -261,6 +273,65 @@ def update_submissions(outputs, operation):
                     submission, output["entry_key"], 'TEXT_ANALYSIS_COMPLETE')
                 # update_question_status(
                 # submission, output["entry_key"], 'REPORT_COMPLETE')
+
+            if operation == "report":
+                assessment_output = output["report"]
+                report_output = assessment_output["report_outputs"]
+                if (assessment_output_row_does_not_exist):
+                    submission.append("assessment_outputs", {
+                        "key_field": key_field,
+                        "word_count": report_output["word_count"],
+                        "lexical_density": report_output["lexical_density"],
+                        "nine_point_score_9": report_output["nine_point_score"],
+                        "conversation_contribution_pct": report_output["conversation_contribution_pct"],
+                        "total_nouns": report_output["total_nouns"],
+                        "total_proper_nouns": report_output["total_proper_nouns"],
+                        "total_verbs": report_output["total_verbs"],
+                        "total_adverbs": report_output["total_adverbs"],
+                        "total_adjectives": report_output["total_adjectives"],
+                        "total_prepositions": report_output["total_prepositions"],
+                        "total_noun_phrases": report_output["total_noun_phrases"],
+                        "total_clause_fragments": report_output["total_clause_fragments"],
+                        "two_letter_words": report_output["two_letter_words"],
+                        "three_letter_words": report_output["three_letter_words"],
+                        "four_letter_words": report_output["four_letter_words"],
+                        "five_letter_words": report_output["five_letter_words"],
+                        "six_letter_words": report_output["six_letter_words"],
+                        "seven_letter_words": report_output["seven_letter_words"],
+                        "eight_letter_words": report_output["eight_letter_words"],
+                        "nine_letter_words": report_output["nine_letter_words"],
+                        "ten_letter_words": report_output["ten_letter_words"],
+                        "greater_than_10_letter_words": report_output["greater_than_10_letter_words"],
+                    })
+                else:
+                    assessment_output_row.word_count = report_output["word_count"]
+                    assessment_output_row.lexical_density = report_output["lexical_density"]
+                    assessment_output_row.nine_point_score_9 = report_output["nine_point_score"]
+                    assessment_output_row.conversation_contribution_pct = report_output[
+                        "conversation_contribution_pct"]
+                    assessment_output_row.total_nouns = report_output["total_nouns"]
+                    assessment_output_row.total_proper_nouns = report_output["total_proper_nouns"]
+                    assessment_output_row.total_verbs = report_output["total_verbs"]
+                    assessment_output_row.total_adverbs = report_output["total_adverbs"]
+                    assessment_output_row.total_adjectives = report_output["total_adjectives"]
+                    assessment_output_row.total_prepositions = report_output["total_prepositions"]
+                    assessment_output_row.total_noun_phrases = report_output["total_noun_phrases"]
+                    assessment_output_row.total_clause_fragments = report_output[
+                        "total_clause_fragments"]
+                    assessment_output_row.two_letter_words = report_output["two_letter_words"]
+                    assessment_output_row.three_letter_words = report_output["three_letter_words"]
+                    assessment_output_row.four_letter_words = report_output["four_letter_words"]
+                    assessment_output_row.five_letter_words = report_output["five_letter_words"]
+                    assessment_output_row.six_letter_words = report_output["six_letter_words"]
+                    assessment_output_row.seven_letter_words = report_output["seven_letter_words"]
+                    assessment_output_row.eight_letter_words = report_output["eight_letter_words"]
+                    assessment_output_row.nine_letter_words = report_output["nine_letter_words"]
+                    assessment_output_row.ten_letter_words = report_output["ten_letter_words"]
+                    assessment_output_row.greater_than_10_letter_words = report_output[
+                        "greater_than_10_letter_words"]
+
+                update_question_status(
+                    submission, output["entry_key"], 'REPORT_COMPLETE')
 
             submission.save()
             frappe.db.commit()
