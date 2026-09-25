d984b37046e3378b530ce42dab440a5aa2950055
Pushpak-Jaiswal <pushpakmjaiswal@gmail.com>
2026-09-25 17:57:44 +0530

    matrix under user control in ELA

--- ela/ela/doctype/elaconfiguration/elaconfiguration.py
+++ ela/ela/doctype/elaconfiguration/elaconfiguration.py
@@ -1,9 +1,16 @@
 # Copyright (c) 2025, IT for Change and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class ELAConfiguration(Document):
-	pass
+	def on_update(self):
+		if not self.language_id_matrix:
+			return
+
+		file_doc = frappe.get_doc("File", {"file_url": self.language_id_matrix})
+		if file_doc.is_private:
+			file_doc.is_private = 0
+			file_doc.save(ignore_permissions=True)
